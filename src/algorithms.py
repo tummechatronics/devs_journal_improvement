6e7575c4f81cf9c82eafc8e9d6ca6155f60b7463
TUMM <tummechatronics38@gmail.com>
2025-02-21 20:28:44 +0100

    add readme and other configs

---
+++ src/algorithms.py
@@ -0,0 +1,16 @@
+def search_bfs(maze: list, current_pivot: set, visited_points: set):
+    # Pseudo code
+    """_summary_
+
+    Args:
+        maze (list): _description_
+        current_pivot (set): _description_
+        visited_points (set): _description_
+    """
+    """
+    new_queue
+    add current_pivot to q
+    mark curret_point as visited
+    while q is not empty:    
+    """
+    ...
