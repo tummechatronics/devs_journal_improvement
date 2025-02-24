9d15dbd464c73f8650484c4151c088e8af5c2178
TUMM <tummechatronics38@gmail.com>
2025-02-24 21:54:19 +0100

    separate readme from journal and remove html add new script for maze generator

--- src/algorithms.py
+++ src/algorithms.py
@@ -1,4 +1,4 @@
-def search_bfs(maze: list, current_pivot: set, visited_points: set):
+def search_bfs(maze: list, current_pivot: int, visited_points: list):
     # Pseudo code
     """_summary_
 
@@ -13,4 +13,21 @@ def search_bfs(maze: list, current_pivot: set, visited_points: set):
     mark curret_point as visited
     while q is not empty:    
     """
+    goal_point: list = []
+    neighbors: list = []
+    q: list = []
+    q.append(current_pivot)
+    visited_points.append(current_pivot)
+    while q:
+        current_pivot = q.pop(0)
+        # add available cells north, south, and west
+        for neighbor in neighbors:
+            if neighbor not in visited_points:
+                current_pivot = neighbor
+                visited_points.append(neighbor)
+                q.append(neighbor)
+                if neighbor is goal_point:
+                    return neighbor
+    return "No Path to goal"
+
     ...
